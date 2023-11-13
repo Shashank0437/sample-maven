7276d72b286fd6db5758ff1cc29a795e138e2241
Shashank0437 <54523830+Shashank0437@users.noreply.github.com>
2023-11-14 03:51:04 +0530

    maven init

---
+++ src/main/java/com/sample/app/UserMgtController.java
@@ -0,0 +1,32 @@
+package com.sample.app;
+
+import com.sample.app.model.User;
+import com.sample.app.service.UserManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserMgtController {
+
+    @Autowired
+    UserManagementService userManagementService;
+
+    @RequestMapping("/users")
+    public User[] getUserList() {
+        return userManagementService.getUserList().toArray(new User[0]);
+    }
+
+    @RequestMapping(value = "/users/add", method = RequestMethod.POST)
+    public void addUser(@RequestBody User user) {
+        userManagementService.addUserList(user);
+    }
+
+    @RequestMapping("/users/{Username}")
+    public String getUserId(@PathVariable("Username") String username){
+        return userManagementService.getUerId(username);
+    }
+}
